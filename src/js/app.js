27f2d46eb62a265cb16308ce8c452a813c3fb346
Marshall Bowers <elliott.codes@gmail.com>
2015-06-03 19:35:18 -0400

    Add Angular

---
+++ src/js/app.js
@@ -0,0 +1,17 @@
+angular.module('micromoira', ['ngRoute']);
+
+angular.module('micromoira')
+    .config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+        'use strict';
+
+        $routeProvider
+            .when('/', {
+                controller: 'HomeController',
+                templateUrl: 'partials/editor.html'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+
+            $locationProvider.html5Mode(true);
+    }]);
---
+++ src/js/controllers.js
@@ -0,0 +1,6 @@
+angular.module('micromoira')
+    .controller('HomeController', ['$scope', function ($scope) {
+        'use strict';
+
+        
+    }]);
