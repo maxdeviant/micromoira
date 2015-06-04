f3f12ad004a81d14570b2e8973803243138901bc
Marshall Bowers <elliott.codes@gmail.com>
2015-06-04 19:52:52 -0400

    Simplify creation process

--- src/js/controllers.js
+++ src/js/controllers.js
@@ -2,15 +2,7 @@ angular.module('micromoira')
     .controller('HomeController', ['$scope','$timeout', function ($scope, $timeout) {
         'use strict';
 
-        var sceneList = [];
-
-        $scope.addScene = function (scene) {
-            scene = scene || createScene();
-
-            $scope.scenes.push(scene);
-
-            initMaterialize();
-        };
+        var sceneCount = 0;
 
         $scope.addExit = function (scene) {
             if (!$scope.canAddExit(scene)) {
@@ -19,12 +11,9 @@ angular.module('micromoira')
 
             var destination = createScene();
 
-            scene.exits.push({
-                label: '',
-                scene: destination.id
-            });
+            scene.exits.push(destination);
 
-            $scope.addScene(destination);
+            addScene(destination);
         };
 
         $scope.canAddExit = function (scene) {
@@ -32,34 +21,25 @@ angular.module('micromoira')
         };
 
         $scope.compile = function () {
-            var scenes = $scope.scenes;
+            var story = $scope.scenes[0];
 
-            console.log(scenes);
+            console.log(story);
         };
 
         function createScene() {
-            var id = guid();
-
-            if (sceneList.indexOf(id) !== -1) {
-                return createScene();
-            }
-
-            sceneList.push(id);
-
             return {
-                id: id,
-                name: 'Scene ' + (sceneList.length),
+                name: 'Scene ' + (++sceneCount),
                 text: '',
                 exits: []
             };
         }
 
-        function guid() {
-            return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-                var r = Math.random() * 16 | 0,
-                    v = c == 'x' ? r : (r & 0x3 | 0x8);
-                return v.toString(16);
-            });
+        function addScene(scene) {
+            scene = scene || createScene();
+
+            $scope.scenes.push(scene);
+
+            initMaterialize();
         }
 
         function initMaterialize() {
