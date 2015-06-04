2ee37cce81d047309e967b48a4928562f2857e63
Marshall Bowers <elliott.codes@gmail.com>
2015-06-04 01:08:35 -0400

    Add scene exits

--- src/js/controllers.js
+++ src/js/controllers.js
@@ -2,30 +2,80 @@ angular.module('micromoira')
     .controller('HomeController', ['$scope','$timeout', function ($scope, $timeout) {
         'use strict';
 
-        $scope.addScene = function () {
-            $scope.scenes.push({
-                name: '',
-                text: '',
-                exits: []
-            });
+        var sceneList = [];
 
-            initCollapsible();
+        $scope.addScene = function (scene) {
+            scene = scene || createScene();
+
+            $scope.scenes.push(scene);
+
+            initMaterialize();
         };
 
-        function initCollapsible() {
+        $scope.addExit = function (scene) {
+            if (!$scope.canAddExit(scene)) {
+                return;
+            }
+
+            var destination = createScene();
+
+            scene.exits.push({
+                label: '',
+                scene: destination.id
+            });
+
+            $scope.addScene(destination);
+        };
+
+        $scope.canAddExit = function (scene) {
+            return scene.exits.length < 2;
+        };
+
+        $scope.compile = function () {
+            var scenes = $scope.scenes;
+
+            console.log(scenes);
+        };
+
+        function createScene() {
+            var id = guid();
+
+            if (sceneList.indexOf(id) !== -1) {
+                return createScene();
+            }
+
+            sceneList.push(id);
+
+            return {
+                id: id,
+                name: 'Scene ' + (sceneList.length),
+                text: '',
+                exits: []
+            };
+        }
+
+        function guid() {
+            return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+                var r = Math.random() * 16 | 0,
+                    v = c == 'x' ? r : (r & 0x3 | 0x8);
+                return v.toString(16);
+            });
+        }
+
+        function initMaterialize() {
             $timeout(function () {
                 $('.collapsible').collapsible();
+                $('textarea[name="text"]').characterCounter();
             });
         }
 
         function init() {
-            $scope.scenes = [{
-                name: 'Scene 1',
-                text: '',
-                exits: []
-            }];
+            var scene = createScene();
+            scene.name = 'Scene 1';
 
-            initCollapsible();
+            $scope.scenes = [scene];
+
+            initMaterialize();
         }
 
         init();
